76cf1cecc2bbf4e46d1d7dbdc1264fc2311ad3f0
Tusnakis <tusnakis666@gmail.com>
2020-04-08 19:51:41 +0200

    modifica index y footer para ajustarlo a móvil

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,8 @@
 $(document).ready(init);
-function init(){
+function init() {
     var comunidadesArr = ["Andalucía", "Aragón", "Asturias", "Baleares", "Canarias", "Cantabria", "Castilla-La Mancha", "Castilla y León", "Cataluña", "Ceuta", "C. Valenciana", "Extremadura", "Galicia", "Madrid", "Melilla", "Murcia", "Navarra", "País Vasco", "La Rioja"];
-    var eleccionArr = ["casos","fallecidos","uci","altas"];
-    for(var i = 0; i < comunidadesArr.length; i++){
+    //var eleccionArr = ["casos", "fallecidos", "uci", "altas"];
+    for (var i = 0; i < comunidadesArr.length; i++) {
         /*if(i < 4){
             $('#idEleccion').append('<option value="' + eleccionArr[i] + '">' + eleccionArr[i] + '</option>');
         }*/
