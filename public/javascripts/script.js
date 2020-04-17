8446dbc4ff81e8c2a3c4d06c0dbd858e33f57c8d
Tusnakis <tusnakis666@gmail.com>
2020-04-17 20:18:49 +0200

    añade decimales al porcentaje y colores para comprobar incremento o decremento

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,4 +8,18 @@ function init() {
         }*/
         $('#idComunidades').append('<option value="' + comunidadesArr[i] + '">' + comunidadesArr[i] + '</option>');
     }
+
+    if($('.divVacio').children().length > 0){
+        for(var x = 1; x < $('table').children().eq(1).children().length; x++){
+            if(x == 1){
+                $('table').children().eq(1).children().eq(x).children().eq(2).css("color","#fc0000");
+            } else {
+                if(parseFloat($('table').children().eq(1).children().eq(x - 1).children().eq(2).text().trim()) < parseFloat($('table').children().eq(1).children().eq(x).children().eq(2).text().trim())){
+                    $('table').children().eq(1).children().eq(x).children().eq(2).css("color","#fc0000");
+                } else {
+                    $('table').children().eq(1).children().eq(x).children().eq(2).css("color","#008a10");
+                }
+            }
+        }
+    }
 }
